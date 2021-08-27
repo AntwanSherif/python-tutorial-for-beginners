69352d974d103b6754472e79591a8300333bc04f
AntwanSherif <antwansherif@gmail.com>
2021-08-28 00:31:54 +0200

    3. type conversion

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 # import l1_variables
-import l2_receive_user_inputs
+# import l2_receive_user_inputs
+import l3_type_conversion
---
+++ l3_type_conversion.py
@@ -0,0 +1,4 @@
+birth_year = input('Birth year: ')
+# print(type(birth_year))
+age = 2021 - int(birth_year)
+print(age)
