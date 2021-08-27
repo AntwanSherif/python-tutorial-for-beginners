f91afe60baddb1ad7d99703f8b9a7129d13c2dd3
AntwanSherif <antwansherif@gmail.com>
2021-08-28 00:45:28 +0200

    4. Basic strings

---
+++ l4_strings.py
@@ -0,0 +1,23 @@
+name = 'Jennifer'
+
+print('nth char: ' + name[1])
+print('nth last char: ' + name[-1])
+print('from nth-index till mth (mth excluded): ' + name[0:4])
+print('until the nth char (assuming starting from 0): ' + name[: 5])
+print('from nth-index till the end: ' + name[3:])
+print('copy string: ' + name[:])
+
+multi_line_string = '''
+Hi John,
+
+Thank you for your email.
+We will try to get to you ASAP.
+
+Have a great day.
+
+
+Best Regards,
+Antwan
+'''
+
+print(multi_line_string)
