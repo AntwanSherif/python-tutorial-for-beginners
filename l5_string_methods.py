d3a85c4d5df92a33017d71162170f839397b13bc
AntwanSherif <antwansherif@gmail.com>
2021-08-29 16:21:37 +0200

    5. String methods

---
+++ l5_string_methods.py
@@ -0,0 +1,8 @@
+course = 'Python for beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.find('P'))
+print(course.replace('Beginner', 'Absolute Beginners'))
+print('Python' in course)
